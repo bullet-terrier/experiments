32390c323483beb7ec2f76b81225fc74d716a4ed
root <root@localhost.localdomain>
2018-04-10 09:05:49 -0400

    dded in graphics.h see the stanford page on it (super handy reference)

---
+++ fun/dev-headers/mtggraphics.h
@@ -0,0 +1,10 @@
+#include <graphics.h>
+#include <comio.h>
+// above here should be the graphical specifics
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Need to set up some baseic  values;
+*/
+
